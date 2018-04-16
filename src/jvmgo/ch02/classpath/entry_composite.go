0a0d0b6f82dc0cb8ee774a1ebc48ec1e36e49457
山葫芦 <shanhulu@2dfire.com>
2018-04-16 15:56:43 +0800

    add

---
+++ src/jvmgo/ch02/classpath/entry_composite.go
@@ -0,0 +1,13 @@
+package classpath
+
+import "strings"
+
+type CompositeEntry []Entry
+
+func newCompositeEntry(pathList string) CompositeEntry {
+	compositeEntry := []Entry{}
+	for _, path := range strings.Split(pathList, pathListSeparator) {
+		entry := newEntry(path)
+		compositeEntry = append(compositeEntry,entry)
+	}
+}
---
+++ src/jvmgo/ch02/classpath/entry_zip2.go
@@ -0,0 +1,70 @@
+package classpath
+
+import (
+	"archive/zip"
+	"path/filepath"
+	"io/ioutil"
+	"errors"
+)
+
+type ZipEntry2 struct {
+	absPath string
+	zipRc   *zip.ReadCloser
+}
+
+func newZipEntry2(path string) *ZipEntry2 {
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		panic(err)
+	}
+	return &ZipEntry2{absPath, nil}
+}
+
+func (self *ZipEntry2) readClass(className string) ([]byte, Entry, error) {
+	if self.zipRc == nil {
+		err := self.openJar()
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	classFile := self.findClass(className)
+	if classFile == nil {
+		return nil, nil, errors.New("class not found: " + className)
+	}
+	data, err := readClass(classFile)
+	return data, self, err
+}
+
+func (self *ZipEntry2) String() string {
+	return self.absPath
+}
+func (self *ZipEntry2) openJar() error {
+	r, err := zip.OpenReader(self.absPath)
+	if err == nil {
+		self.zipRc = r
+	}
+	return err
+}
+func (self *ZipEntry2) findClass(className string) *zip.File {
+	for _, f := range self.zipRc.File {
+		if f.Name == className {
+			return f
+		}
+	}
+	return nil
+}
+
+func readClass(classFile *zip.File) ([]byte, error) {
+	rc, err := classFile.Open()
+	if err != nil {
+		return nil, err
+	}
+	//read class data
+	data, err := ioutil.ReadAll(rc)
+	rc.Close()
+	if err != nil {
+		return nil, err
+	}
+	return data, err
+}
